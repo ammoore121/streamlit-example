0695a37db2c037fa316e9de4f815798a39307baa
ammoore121 <95384055+ammoore121@users.noreply.github.com>
2023-07-07 17:27:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 #the app will reflect save and commits
+
 
 
 st.title('Blue Dot :blue[Sports] :)') 
@@ -8,3 +10,9 @@ st.title('Blue Dot :blue[Sports] :)')
 st.write('Hello')
 
 
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
