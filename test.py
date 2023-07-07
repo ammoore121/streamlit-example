38c46eed655ec088bc4761b156178ad945f0b1fe
ammoore121 <95384055+ammoore121@users.noreply.github.com>
2023-07-07 17:11:47 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import streamlit as st
 #the app will reflect save and commits
 
 
-def app():
-    st.title('Blue Dot :blue[Sports] :)') 
+st.title('Blue Dot :blue[Sports] :)') 
 
 st.write(app)
 st.write('Hello')
