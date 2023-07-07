a8367c03eb46cbdb9312b92f457511a290394a97
ammoore121 <95384055+ammoore121@users.noreply.github.com>
2023-07-07 18:12:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,27 +7,10 @@ import pandas as pd
 st.title('Blue Dot :blue[Sports] :)') 
 
 
-
-
-
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
-
-df
-
-st.line_chart(df)
-
-
-
-st.header('st.button')
-
-if st.button('See Table'):
-     df
-else:
-     st.write()
-
 
 add_sidebar = st.sidebar.selectbox('Menu',('Place Bet','My Bets','My Bank','Test Env'))
 
@@ -35,9 +18,9 @@ if add_sidebar == "Test Env":
   df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]})
-
   df
   st.line_chart(df)
+  
   if st.button('Click for TN'):
        st.write('##These Nuts)'
   else:
