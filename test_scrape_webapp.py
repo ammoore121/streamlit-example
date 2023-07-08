d4ccd36375445de5c336f9b5c127decdd8da0ee4
Aaron Moore <ammoore121@gmail.com>
2023-07-07 20:40:14 -0400

    accountBalance input starting value 0

--- test_scrape_webapp.py
+++ test_scrape_webapp.py
@@ -34,7 +34,7 @@ else:
 
 if add_sidebar == "Place Bet":
      
-     accountBalance = st.number_input('Insert Account Balance')
+     accountBalance = st.number_input('Insert Account Balance',value=0)
      username = 'amoore'
      if st.button('Execute'):
         url = "https://content.unabated.com/markets/game-odds/v_gameodds.json"
